56dd3b556036c1cf3aaf3fb49e5c558783c59daf
Edric Liu <edrliu@cedar1.int.cedar.computecanada.ca>
2025-02-09 08:01:28 -0800

    .

--- evaluation.py
+++ evaluation.py
@@ -4,17 +4,8 @@ import transformers.models.llama.modeling_llama as llama
 from transformers import LlamaTokenizer
 from datasets import load_dataset
 
+
 from evaluation_hook import ForwardHook
-
-# normal_model = llama.LlamaForCausalLM(llama.LlamaConfig())
-# normal_model.from_pretrained("meta-llama/Llama-2-7b-hf")
-# normal_hook = ForwardHook(normal_model)
-# normal_model.eval()
-
-# sliced_model = llama.LlamaForCausalLM(llama.LlamaConfig())
-sliced_model = torch.load("/home/edrliu/projects/def-mmehride/edrliu/TransformerCompression2/out/Llama-2-7b-hf_0.25.pt")
-sliced_hook = ForwardHook(sliced_model)
-sliced_model.eval()
 
 wiki = load_dataset("wikipedia", "20200501.en")
 
@@ -23,15 +14,8 @@ tokenizer.pad_token = tokenizer.eos_token
 inputs = tokenizer(wiki["text"], return_tensors="pt", padding=True, truncation=True).to("cuda")
 
 with torch.no_grad():
-    # normal_model(**inputs)
     sliced_model(**inputs)
 
 torch.save(sliced_hook, "hook_output.pt")
 
 sliced_hook.close()
-
-# for normal_out, sliced_out in zip(normal_hook.out, sliced_hook.out):
-#     print(f"normal: {normal_out}")
-#     print(f"sliced: {sliced_out}")
-# normal_hook.close()
-# sliced_hook.close()
