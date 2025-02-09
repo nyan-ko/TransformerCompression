87bb8ca6e069e1a0a649b3c55ba09cb1a1f30f93
nyan-ko <dumbstupidcatboy@gmail.com>
2025-02-09 11:35:08 -0500

    .

--- evaluation.py
+++ evaluation.py
@@ -4,8 +4,11 @@ import transformers.models.llama.modeling_llama as llama
 from transformers import LlamaTokenizer
 from datasets import load_dataset
 
+from slicegpt import load_sliced_model
 
 from evaluation_hook import ForwardHook
+
+import sys
 
 wiki = load_dataset("wikipedia", "20200501.en")
 
@@ -13,8 +16,11 @@ tokenizer = LlamaTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
 tokenizer.pad_token = tokenizer.eos_token
 inputs = tokenizer(wiki["text"], return_tensors="pt", padding=True, truncation=True).to("cuda")
 
+model_adapter, _ = load_sliced_model("meta-llama/Llama-2-7b-hf", sys.argv[1], sparsity=sys.argv[2], token=sys.argv[3])
+sliced_hook = ForwardHook(model_adapter.model)
+
 with torch.no_grad():
-    sliced_model(**inputs)
+    model_adapter.model(**inputs)
 
 torch.save(sliced_hook, "hook_output.pt")
 
